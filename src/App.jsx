07c08d7e1fc57ae61da8e2f962e3539c09c21e6d
rez1-dev <rezwan.mahmud.007@gmail.com>
2025-09-22 12:39:36 +0600

    added just one component and many more to go

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1>World Tour With React!</h1>
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>In the countries</h3>
+        </div>
+    );
+};
+
+export default Countries;
